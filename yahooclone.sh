7ee9e48db8c71910a2481a5ee7739ae5db124414
zzpay1110 <50121531+zzpay1110@users.noreply.github.com>
2019-05-02 03:52:11 +0700

    Add files via upload
    Yahooclone installer

---
+++ yahooclone.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clear
+echo "";
+read -p "@Yahooclone******Silahkan Enter Untuk Menginstall Bahan :) " a;
+sleep 2
+echo " *******Installer by @zzpay*******"
+sleep 2
+echo "[+] Tunggu Sebentar Sedang Menginstall PYTHON2...";
+pkg install python2
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall GIT...";
+pkg install git
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall REQUESTS...";
+pip2 install requests
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall MECHANIZE...";
+pip2 install mechanize
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall YAHOOCLONE...";
+unzip Yahooclone.zip -o
+sleep 1
+echo "[+] Installing...";
+sleep 2
+echo "[+] Install Selesai,Silahkan Enter untuk memulai...";
+python2 start.py
